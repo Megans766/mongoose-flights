a15f316b1b87b1f487480ae3ec97100a7aa57c2b
Megan Smith <megans766@gmail.com>
2023-01-09 21:06:42 -0500

    update controllers & views

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,23 @@
+import { Flight } from '../models/flight.js'
+
 function newFLight(req, res) {
   res.render('flights/new', {
     title: 'Add Flight'
   })
 }
 
+function create(req, res) {
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights/new')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
 export {
-  newFLight as new
+  newFLight as new,
+  create
 }
